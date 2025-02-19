ca04057c72a7ad48e6cdb4eb7c5bfeb37788c786
baymax475 <srija.adepu4795@gmail.com>
2025-02-19 10:44:02 -0600

    Your commit message

--- config.py
+++ config.py
@@ -2,5 +2,5 @@ import os
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'your-secret-key'
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///diabetes.db'
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///diabetes.db'
     SQLALCHEMY_TRACK_MODIFICATIONS = False
